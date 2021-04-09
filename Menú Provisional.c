f422491601b82a53f548174636d850cb2301f53b
Luislagu <79968609+Luislagu@users.noreply.github.com>
2021-04-09 23:57:22 +0200

    Menú provisional
    Este es el menú que utilizaremos por el momento.

--- Menú Provisional.c
+++ Menú Provisional.c
@@ -3,42 +3,42 @@
 int main()
 {
     char Operacion;
-  printf("Escribe que Operacion quieres, pulsa "Q" para acabar. \n");
+    printf("Escribe que Operacion quieres, pulsa Q para acabar. \n");
 
-  while(Operacion!='Q')
-  {
-      scanf("%c",&Operacion);
-      switch(Operacion)
-      {
-      case 'D':
-        printf("Dominio\n");
-        break;
+    while(Operacion!='Q')
+    {
+        scanf("%c",&Operacion);
+        switch(Operacion)
+        {
+        case 'D':
+            printf("Dominio\n");
+            break;
         case 'd':
-        printf("Derivada\n");
-        break;
+            printf("Derivada\n");
+            break;
         case 'R':
-        printf("Recorrido\n");
-        break;
+            printf("Recorrido\n");
+            break;
         case 'F':
-        printf("Funcion InversaHola\n");
-        break;
+            printf("Funcion InversaHola\n");
+            break;
         case 'P':
-        printf("Puntos de corte con los ejes\n");
-        break;
+            printf("Puntos de corte con los ejes\n");
+            break;
         case 'S':
-        printf("Signo\n");
-        break;
+            printf("Signo\n");
+            break;
         case 'E':
-        printf("Extremos\n");
-        break;
+            printf("Extremos\n");
+            break;
         case 'p':
-        printf("Puntos de Inflexion\n");
-        break;
+            printf("Puntos de Inflexion\n");
+            break;
         case 'G':
-        printf("Gráfica\n");
-        break;
+            printf("Gráfica\n");
+            break;
       }
-  }
-  return 0;
+   }
+   return 0;
 }
 
