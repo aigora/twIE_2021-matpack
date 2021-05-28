e2d7055ea8d02773102299c2fbfbed2e388ab17d
Luislagu <79968609+Luislagu@users.noreply.github.com>
2021-05-29 00:00:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,13 +3,6 @@
 #include <math.h>
 #include "opciones.h"
 
-//Esta estructura define una condición del tipo <=, <, >=, > o =.
-typedef struct
-{
-    int condic;
-    float num;
-}condicion;
-
 int main()
 {
     primer_menu();
